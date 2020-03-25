ed6cc8ba692a371188f43d3b827cbf76edfbf551
Noordeen Malango <noordinmalango@gmail.com>
2020-03-25 08:50:35 +0200

    simple linear regression

---
+++ README.md
@@ -0,0 +1,1 @@
+# ibmcode_ai
---
+++ simple_linear_regression.py
@@ -0,0 +1,63 @@
+#import the necessary packages 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import pylab as pl
+import numpy as np 
+
+df = pd.read_csv('FuelConsumptionCo2.csv')
+
+#data exploration
+print(df.describe())
+print(df.columns)
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+print(cdf.head(9))
+
+viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
+viz.hist()
+plt.show()
+
+# plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')
+# plt.xlabel("FUELCONSUMPTION_COMB")
+# plt.ylabel("Emission")
+# plt.show()
+
+# plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
+# plt.xlabel("Engine size")
+# plt.ylabel("Emission")
+# plt.show()
+
+# plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS,  color='blue')
+# plt.xlabel("Cylinder")
+# plt.ylabel("Emission")
+# plt.show()
+#Lets split our dataset into train and test sets, 80% of the entire data for training, and the 20% 
+#for testing. We create a mask to select random rows using np.random.rand() function: 
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+
+from sklearn import linear_model
+regr = linear_model.LinearRegression()
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit (train_x, train_y)
+# The coefficients
+print ('Coefficients: ', regr.coef_)
+print ('Intercept: ',regr.intercept_)
+
+
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+#evaluation
+from sklearn.metrics import r2_score
+
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_hat = regr.predict(test_x)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_hat - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_hat - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y_hat , test_y) )
