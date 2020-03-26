0e8b1ec6db6d591f3d51f07ef96f281625b22159
Noordeen Malango <noordinmalango@gmail.com>
2020-03-26 23:31:30 +0200

    polynomial regression

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -45,3 +45,11 @@ regr.fit (x, y)
 print ('Coefficients: ', regr.coef_)
 
 
+y_hat= regr.predict(test[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
+x = np.asanyarray(test[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
+y = np.asanyarray(test[['CO2EMISSIONS']])
+print("Residual sum of squares: %.2f"
+      % np.mean((y_hat - y) ** 2))
+
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % regr.score(x, y))
---
+++ polynomial_regression.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+
+df = pd.read_csv("FuelConsumptionCo2.csv")
+
+print(df.head())
